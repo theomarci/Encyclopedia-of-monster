31e801f6d9984ccd4c54b8be40026f58491400ae
Théo <theos9mar@gmail.com>
2024-02-20 15:26:12 +0100

    Test Hello world

--- Encyclopediae/theEncyclopedia/HomePage/views.py
+++ Encyclopediae/theEncyclopedia/HomePage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def HomePage(request):
+    template = loader.get_template('HomePage.html')
+    return HttpResponse(template.render())
