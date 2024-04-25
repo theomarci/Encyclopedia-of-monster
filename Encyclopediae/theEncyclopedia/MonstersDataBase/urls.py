ccfd0e3fa23d65b2b7f1bfe9f923a52a9889e6da
Théo <theos9mar@gmail.com>
2024-04-25 15:25:04 +0200

    little save by security

--- Encyclopediae/theEncyclopedia/MonstersDataBase/urls.py
+++ Encyclopediae/theEncyclopedia/MonstersDataBase/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# The different path to the MonstersDataBase app
+# For each path, there is the function in views that load the good templates, data.
+
 urlpatterns = [
     path('home/', views.Home),
     path('list/', views.search),
